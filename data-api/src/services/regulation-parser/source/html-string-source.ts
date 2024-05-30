db0ef1100a7470d69da41957c099f7e515793103
Victor Karalkin <mr.coast72@gmail.com>
2024-05-29 21:56:13 -0400

    Fix eslint

--- data-api/src/services/regulation-parser/source/html-string-source.ts
+++ data-api/src/services/regulation-parser/source/html-string-source.ts
@@ -4,6 +4,7 @@ import { PlainObject } from '../parser';
 
 const DOM_NODE_ELEMENT = 1;
 const DOM_NODE_TEXT = 3;
+
 export default class HtmlStringSource extends Source {
     public getPlainData(inputData: string): object {
         // Cheerio works with the whole HTML document in memory, --max_old_space_size=1536 set in package.json
@@ -20,21 +21,21 @@ export default class HtmlStringSource extends Source {
         const plainObject: PlainObject = {};
       
         switch (node.nodeType) {
-          case DOM_NODE_ELEMENT:
-            plainObject.tagName = node.tagName.toLowerCase();
-            plainObject.attributes = {};
-            for (const attr of node.attributes) {
-              plainObject.attributes[attr.name] = attr.value;
-            }
-            plainObject.children = [];
-            for (const child of node.childNodes) {
-              plainObject.children.push(this.convertNodeToPlainObject(child as HTMLElement));
-            }
-            break;
+            case DOM_NODE_ELEMENT:
+                plainObject.tagName = node.tagName.toLowerCase();
+                plainObject.attributes = {};
+                for (const attr of node.attributes) {
+                    plainObject.attributes[attr.name] = attr.value;
+                }
+                plainObject.children = [];
+                for (const child of node.childNodes) {
+                    plainObject.children.push(this.convertNodeToPlainObject(child as HTMLElement));
+                }
+                break;
           case DOM_NODE_TEXT:
-            // node object returned by Cheerio uses "data" property instead of "textContent"
-            plainObject.textContent = (node as any).data.trim();
-            break;
+              // node object returned by Cheerio uses "data" property instead of "textContent"
+              plainObject.textContent = (node as any).data.trim();
+              break;
         }
       
         return plainObject;
